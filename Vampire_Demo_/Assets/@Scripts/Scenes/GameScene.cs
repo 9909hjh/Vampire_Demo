157191601b663be2c295d1dc949de8cd6257f4b2
9909hjh <9909hjh@gmail.com>
2024-07-04 23:56:37 +0900

    어드레서블 방식과 리소드 로드 방식 섞어서 사용

--- Vampire_Demo_/Assets/@Scripts/Scenes/GameScene.cs
+++ Vampire_Demo_/Assets/@Scripts/Scenes/GameScene.cs
@@ -4,12 +4,40 @@ using UnityEngine;
 
 public class GameScene : MonoBehaviour
 {
+    GameObject _snake;
+    GameObject _goblin;
+    
+
 
     void Start()
     {
-        
+        Managers.Resource.LoadAllAsync<GameObject>("Prefabs", (key, count, totalCount) =>
+        {
+            Debug.Log($"{key} {count}/{totalCount}");
+
+            if(count == totalCount)
+            {
+                StartLoaded();
+            }
+        });
     }
 
+    void StartLoaded()
+    {
+        GameObject prefab = Managers.Resource.Load<GameObject>("Player.prefab");
+
+        //Managers.Resource.LoadAsync<GameObject>("Snake_01", (go) =>
+        //{
+        //    // Todo
+        //});
+
+        GameObject go = new GameObject() { name = "@Monsters" };
+        _snake.transform.parent = go.transform;
+        _goblin.transform.parent = go.transform;
+
+        //prefab.AddComponent<PlayerController>();
+        //Camera.main.GetComponent<CameraController>()
+    }
 
     void Update()
     {
