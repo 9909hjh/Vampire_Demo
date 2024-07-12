6693d3456dab55d0af965f0faa69b235b0c6a503
9909hjh <9909hjh@gmail.com>
2024-07-12 20:08:53 +0900

    스킬카드 3개 소환 및 카드 눌림 확인

--- Vampire_Demo_/Assets/@Scripts/UI/UI_SkillCardItem.cs
+++ Vampire_Demo_/Assets/@Scripts/UI/UI_SkillCardItem.cs
@@ -4,5 +4,22 @@ using UnityEngine;
 
 public class UI_SkillCardItem : UI_Base
 {
-    
+    // 어떤 스킬?
+    // 몇 레벨?
+    // 데이터시트?
+    int _templateID;
+    Data.SkillData _skillData;
+
+    public void SetInfo(int templateID)
+    {
+        _templateID = templateID;
+
+        Managers.Data.SkillDic.TryGetValue(templateID, out _skillData);
+    }
+
+    public void OnClickItem()
+    {
+        Debug.Log("OnClickItem");
+        Managers.UI.ClosePopup();
+    }
 }
